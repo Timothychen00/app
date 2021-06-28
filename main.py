b0bc198a4e4753b3c2520c36589f168fd123a608
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2021-06-28 21:54:18 +0800

    nw

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#! -*- encoding:utf-8 -*-
+from flask import Flask,render_template
+app=Flask(__name__)#檢測執行時的模組，可以是main
+
+@app.route("/")
+def home():
+    return render_template("main.html")
+
+if __name__=="__main__":
+    app.run()#啟動伺服器
