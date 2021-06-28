75ffb856c2436d4767de5133a4b75810b4fc0c31
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2021-06-28 22:50:59 +0800

    s

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app=Flask(__name__)#檢測執行時的模組，可以是main
 
 @app.route("/")
 def home():
-    return render_template("main.html")
+    return render_template("a.html")
 
 if __name__=="__main__":
     app.run()#啟動伺服器
