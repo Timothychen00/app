7afdab37dc01512ba41d52302ae2e7667f67ea21
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2021-07-12 23:46:17 +0800

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app=Flask(__name__)#檢測執行時的模組，可以是main
 
 @app.route("/")
 def home():
-    return render_template("a.html")
+    return render_template("main_test.html")
 
 def run():
     app.run(host="0.0.0.0",port="80")
