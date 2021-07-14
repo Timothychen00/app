5c6679792e218bf47aaa20b217d39f5c0165a6b2
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2021-07-14 10:03:35 +0800

    v0.0.2-revise

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app=Flask(__name__)#檢測執行時的模組，可以是main
 
 @app.route("/")
 def home():
-    return render_template("v0.0.2-RWD.html")
+    return render_template("v0.0.2-RWD-revise.html")
 
 def run():
     app.run(host="0.0.0.0",port="80")
